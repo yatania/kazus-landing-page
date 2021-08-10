9bc914b7384da00de388bb11b7c6f9219674e2cb
Tetiana Yasko <taniayasko2015@gmail.com>
2021-08-10 15:41:27 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const toggler = document.querySelector('.header__toggler');
+const header = document.querySelector('.header');
+
+toggler.addEventListener('click', () => {
+  header.classList.toggle('menu-open');
+});
