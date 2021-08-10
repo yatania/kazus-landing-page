f81441ef9cc4f60c0465a991f667c5ae80093177
Tetiana Yasko <taniayasko2015@gmail.com>
2021-08-10 19:56:20 +0300

    added some interactive to the page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,27 @@
 'use strict';
 
+const page = document.querySelector('.page');
 const toggler = document.querySelector('.header__toggler');
 const header = document.querySelector('.header');
+const basket = document.querySelector('.header__basket-button');
+const modal = document.querySelector('.modal');
+const list = document.querySelector('.nav__list');
+const closeButton = document.querySelector('.modal__closer');
 
 toggler.addEventListener('click', () => {
   header.classList.toggle('menu-open');
+  page.classList.toggle('page--hidden');
+});
+
+list.addEventListener('click', () => {
+  header.classList.remove('menu-open');
+  page.classList.remove('page--hidden');
+});
+
+basket.addEventListener('click', () => {
+  modal.classList.toggle('modal--opener');
+});
+
+closeButton.addEventListener('click', () => {
+  modal.classList.remove('modal--opener');
 });
