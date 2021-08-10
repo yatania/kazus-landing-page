5f4c7001895151dd46f666e687a14032cc7b517d
Tetiana Yasko <taniayasko2015@gmail.com>
2021-08-10 22:10:03 +0300

    added some hover effects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,9 @@ const basket = document.querySelector('.header__basket-button');
 const modal = document.querySelector('.modal');
 const list = document.querySelector('.nav__list');
 const closeButton = document.querySelector('.modal__closer');
+const nextButton = document.querySelector('.shop__button-next');
+const previousButton = document.querySelector('.shop__button-prev');
+const numbers = document.querySelectorAll('.shop__page');
 
 toggler.addEventListener('click', () => {
   header.classList.toggle('menu-open');
@@ -25,3 +28,40 @@ basket.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   modal.classList.remove('modal--opener');
 });
+
+let i = 2;
+
+const activeNumber = (n) => {
+  for (const number of numbers) {
+    number.classList.remove('shop__page--active');
+  }
+  numbers[n].classList.add('shop__page--active');
+};
+
+const nextNumber = () => {
+  return () => {
+    if (i === numbers.length - 1) {
+      i = 0;
+      activeNumber(i);
+    } else {
+      i++;
+      activeNumber(i);
+    }
+  };
+};
+
+const prevNumber = () => {
+  return () => {
+    if (i === 0) {
+      i = numbers.length - 1;
+      activeNumber(i);
+    } else {
+      i--;
+      activeNumber(i);
+    }
+  };
+};
+
+nextButton.addEventListener('click', nextNumber());
+
+previousButton.addEventListener('click', prevNumber());
